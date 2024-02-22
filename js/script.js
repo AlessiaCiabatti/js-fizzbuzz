7396e843c96268c7ae92b7131f967d3b65060adf
AlessiaCiabatti <alessiaciaba@gmail.com>
2024-02-22 15:22:06 +0100

    [add] js-&&

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
 for(let i = 1; i <= 100; i++){
   console.log(i);
-  if(i % 3 === 0){
-    console.log('Fizz');
+  // va per prima perchè è più specifica
+  if(i % 3 === 0 && i % 5 === 0){
+    console.log('ciao');
   }else if (i % 5 === 0){
     console.log('Buzz');
+  }else if(i % 3 === 0){
+    console.log('Fizz');
   }
 }
